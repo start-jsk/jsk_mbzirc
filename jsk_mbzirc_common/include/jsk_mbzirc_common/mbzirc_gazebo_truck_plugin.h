c90c29da9761b24655b7d5869e9edd3e58ba7713
Kei Okada <kei.okada@gmail.com>
2016-01-13 19:09:51 +0900

    fix mbzirc_gazebo_truck_plugin, support scroe publisher

--- jsk_mbzirc_common/include/jsk_mbzirc_common/mbzirc_gazebo_truck_plugin.h
+++ jsk_mbzirc_common/include/jsk_mbzirc_common/mbzirc_gazebo_truck_plugin.h
@@ -6,12 +6,15 @@
 
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/physics/physics.hh>
+#include <gazebo/physics/RayShape.hh>
+#include <gazebo/physics/Collision.hh>
 #include <gazebo/common/Events.hh>
 #include <gazebo/common/common.hh>
 
 #include <stdio.h>
 #include <ros/callback_queue.h>
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 namespace gazebo
 {
@@ -22,9 +25,9 @@ public:
   GazeboTruck();
   virtual ~GazeboTruck();
 
-  static const float DISTANCE_THRE = 14.14214; // the end of stright road(x or y coordinate)
-  static const float CIRCLE_CENTER = 28.28427; 
-  static const float VELOCITY = 4.16667; 
+  // http://www.mbzirc.com/assets/files/MBZIRC-Challenge-Description-Document-V2-7SEP2015.pdf
+  static const float CIRCLE_RADIUS = 20.0;
+  static const float CIRCLE_DISTANCE = 55.0;
 
 protected:
   virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
@@ -41,15 +44,14 @@ private:
   std::string namespace_;
 
   ros::NodeHandle* node_handle_;
-
+  ros::Publisher pub_score_;
   ros::Time state_stamp_;
 
   event::ConnectionPtr update_connection_;
 
-  float x_;
-  float y_;
-  float direction_;
-  bool circle_;
+  double traversed_;
+  common::Time last_time_;
+  bool terminated_;
 
 };
 
