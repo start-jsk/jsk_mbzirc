4ac4e70d8e05bbad1dce519d987413db2010ce7c
Krishneel <krishneel.chand.chaudhary@gmail.com>
2016-04-24 17:26:14 +0900

    syntax

--- jsk_mbzirc_tasks/include/jsk_mbzirc_tasks/uav_detect_landing_region.h
+++ jsk_mbzirc_tasks/include/jsk_mbzirc_tasks/uav_detect_landing_region.h
@@ -3,6 +3,8 @@
 #ifndef _UAV_DETECT_LANDING_REGION_H_
 #define _UAV_DETECT_LANDING_REGION_H_
 
+#include <omp.h>
+
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <message_filters/subscriber.h>
@@ -15,20 +17,15 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <cv_bridge/cv_bridge.h>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/video/video.hpp>
-#include <opencv2/video/background_segm.hpp>
-
-#include <boost/thread/mutex.hpp>
-
 #include <geometry_msgs/PolygonStamped.h>
 #include <jsk_recognition_msgs/Rect.h>
 
 #include <jsk_mbzirc_tasks/histogram_of_oriented_gradients.h>
 
-#include <omp.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <boost/thread/mutex.hpp>
 
 class UAVLandingRegion {
 
