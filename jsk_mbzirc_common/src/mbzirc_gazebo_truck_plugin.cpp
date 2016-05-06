c54e3260cf0c9af5256f03df8a96828a16da5e78
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-05-06 22:51:58 +0900

    [jsk_mbzirc_common/src/mbzirc_gazebo_truck_plugin.cpp] fix: remove unused node handler

--- jsk_mbzirc_common/src/mbzirc_gazebo_truck_plugin.cpp
+++ jsk_mbzirc_common/src/mbzirc_gazebo_truck_plugin.cpp
@@ -88,7 +88,6 @@ void GazeboTruck::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   node_handle_ = new ros::NodeHandle(namespace_);
   pub_score_ = node_handle_->advertise<std_msgs::String>("score", 1, true);  // set latch true
   pub_time_ = node_handle_->advertise<std_msgs::String>("remaining_time", 1);
-  ros::NodeHandle param_handle(*node_handle_, "controller");
 
   update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboTruck::Update, this));
 }
