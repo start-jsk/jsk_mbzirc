5af5643d4fcb29de28f91a817a87e6fc1effe6a7
Krishneel <krishneel.chand.chaudhary@gmail.com>
2016-05-06 21:57:34 +0900

    changed chens files

--- jsk_mbzirc_tasks/src/img2pointcloud/uav_img2pointcloud.cpp
+++ jsk_mbzirc_tasks/src/img2pointcloud/uav_img2pointcloud.cpp
@@ -116,65 +116,74 @@ void uav_img2pointcloud::p2p(const sensor_msgs::ImageConstPtr& img,
     for(int i = 0; i < 3; i++)
         for(int j = 0; j < 4; j++)
             P.at<double>(i,j) = cam_info->P.at(i*4+j);
-    //however, this P is in camera coordinate..
+    // however, this P is in camera coordinate..
     extrisic.getOpenGLMatrix(extrisic_data);
-    cv::Mat E_MAT(4,4,CV_64FC1,extrisic_data);
+    cv::Mat E_MAT(4, 4, CV_64FC1, extrisic_data);
     P_Mat_G = P*(E_MAT.t());
     // now is the ground, namely, world coordinate
-    double a[4],b[4],c[4];
-    a[0] = P_Mat_G.at<double>(0,0); a[1] = P_Mat_G.at<double>(0,1); a[2] = P_Mat_G.at<double>(0,2); a[3] = P_Mat_G.at<double>(0,3);
-    b[0] = P_Mat_G.at<double>(1,0); b[1] = P_Mat_G.at<double>(1,1); b[2] = P_Mat_G.at<double>(1,2); b[3] = P_Mat_G.at<double>(1,3);
-    c[0] = P_Mat_G.at<double>(2,0); c[1] = P_Mat_G.at<double>(2,1); c[2] = P_Mat_G.at<double>(2,2); c[3] = P_Mat_G.at<double>(2,3);
+    double a[4], b[4], c[4];
+    a[0] = P_Mat_G.at<double>(0, 0);
+    a[1] = P_Mat_G.at<double>(0, 1);
+    a[2] = P_Mat_G.at<double>(0, 2);
+    a[3] = P_Mat_G.at<double>(0, 3);
+    b[0] = P_Mat_G.at<double>(1, 0);
+    b[1] = P_Mat_G.at<double>(1, 1);
+    b[2] = P_Mat_G.at<double>(1, 2);
+    b[3] = P_Mat_G.at<double>(1, 3);
+    c[0] = P_Mat_G.at<double>(2, 0);
+    c[1] = P_Mat_G.at<double>(2, 1);
+    c[2] = P_Mat_G.at<double>(2, 2);
+    c[3] = P_Mat_G.at<double>(2, 3);
     std::clock_t start;
     double duration;
     start = std::clock();
+    
+//     //gpu
+// #if defined(GPU_EN)
+//     process_in_cuda(a, b, c, &cvimg, &Pointcloud);
+// #else
+//     //cpu
+//     for(int i=0;i<Pointcloud.height;i++)
+//         for(int j=0;j<Pointcloud.width;j++)
+//         {
+//             /*  using matrix (so slow!!!!!!!!!!!!)
+//             cv::Mat A(2,2,CV_64FC1),A_inv(2,2,CV_64FC1),bv(2,1,CV_64FC1);
+//             A.at<double>(0,0) = j*c[0] - a[0]; A.at<double>(0,1) = j*c[1] - a[1];
+//             A.at<double>(1,0) = i*c[0] - b[0]; A.at<double>(1,1) = i*c[1] - b[1];
+//             bv.at<double>(0,0) = a[2]*Ground_Z + a[3] - j*c[2]*Ground_Z - j*c[3];
+//             bv.at<double>(1,0) = b[2]*Ground_Z + b[3] - i*c[2]*Ground_Z - i*c[3];
+//             cv::invert(A,A_inv,cv::DECOMP_LU);
+//             cv::Mat Point = A_inv*bv;
+//             Pointcloud.points[i*Pointcloud.width+j].x = (float)Point.at<double>(0,0);
+//             Pointcloud.points[i*Pointcloud.width+j].y = (float)Point.at<double>(1,0);
+//             Pointcloud.points[i*Pointcloud.width+j].z = (float)Ground_Z;
+//             */
+//             //directly calculation make it faster, next step is to parallize it
+//             float A[2][2],bv[2];
+//             A[0][0] = j*c[0] - a[0]; A[0][1] = j*c[1] - a[1];
+//             A[1][0] = i*c[0] - b[0]; A[1][1] = i*c[1] - b[1];
+//             bv[0]= a[2]*Ground_Z + a[3] - j*c[2]*Ground_Z - j*c[3];
+//             bv[1] = b[2]*Ground_Z + b[3] - i*c[2]*Ground_Z - i*c[3];
+//             float DomA = A[1][1]*A[0][0]-A[0][1]*A[1][0];
+//             Pointcloud.points[i*Pointcloud.width+j].x = (A[1][1]*bv[0]-A[0][1]*bv[1])/DomA;
+//             Pointcloud.points[i*Pointcloud.width+j].y = (A[0][0]*bv[1]-A[1][0]*bv[0])/DomA;
+//             Pointcloud.points[i*Pointcloud.width+j].z = (float)Ground_Z;
 
-    //gpu
-#if defined(GPU_EN)
-    process_in_cuda(a, b, c, &cvimg, &Pointcloud);
-#else
-    //cpu
-    for(int i=0;i<Pointcloud.height;i++)
-        for(int j=0;j<Pointcloud.width;j++)
-        {
-            /*  using matrix (so slow!!!!!!!!!!!!)
-            cv::Mat A(2,2,CV_64FC1),A_inv(2,2,CV_64FC1),bv(2,1,CV_64FC1);
-            A.at<double>(0,0) = j*c[0] - a[0]; A.at<double>(0,1) = j*c[1] - a[1];
-            A.at<double>(1,0) = i*c[0] - b[0]; A.at<double>(1,1) = i*c[1] - b[1];
-            bv.at<double>(0,0) = a[2]*Ground_Z + a[3] - j*c[2]*Ground_Z - j*c[3];
-            bv.at<double>(1,0) = b[2]*Ground_Z + b[3] - i*c[2]*Ground_Z - i*c[3];
-            cv::invert(A,A_inv,cv::DECOMP_LU);
-            cv::Mat Point = A_inv*bv;
-            Pointcloud.points[i*Pointcloud.width+j].x = (float)Point.at<double>(0,0);
-            Pointcloud.points[i*Pointcloud.width+j].y = (float)Point.at<double>(1,0);
-            Pointcloud.points[i*Pointcloud.width+j].z = (float)Ground_Z;
-            */
-            //directly calculation make it faster, next step is to parallize it
-            float A[2][2],bv[2];
-            A[0][0] = j*c[0] - a[0]; A[0][1] = j*c[1] - a[1];
-            A[1][0] = i*c[0] - b[0]; A[1][1] = i*c[1] - b[1];
-            bv[0]= a[2]*Ground_Z + a[3] - j*c[2]*Ground_Z - j*c[3];
-            bv[1] = b[2]*Ground_Z + b[3] - i*c[2]*Ground_Z - i*c[3];
-            float DomA = A[1][1]*A[0][0]-A[0][1]*A[1][0];
-            Pointcloud.points[i*Pointcloud.width+j].x = (A[1][1]*bv[0]-A[0][1]*bv[1])/DomA;
-            Pointcloud.points[i*Pointcloud.width+j].y = (A[0][0]*bv[1]-A[1][0]*bv[0])/DomA;
-            Pointcloud.points[i*Pointcloud.width+j].z = (float)Ground_Z;
-
-            //fill the color info
-            uint8_t rgb[4];
-            rgb[0] = cvimg.at<cv::Vec3b>(i,j)[0];
-            rgb[1] = cvimg.at<cv::Vec3b>(i,j)[1];
-            rgb[2] = cvimg.at<cv::Vec3b>(i,j)[2];
-            rgb[3] = 0;
-            Pointcloud.points[i*Pointcloud.width+j].rgb = *(float *)(rgb);
-        }
-#endif
-    //get the duration....
-    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    std::cout<<"process_time is "<< duration << " second" <<'\n';
-    //publish pointcloud
-    pcl::toROSMsg(Pointcloud,cloud_msg);
-    pointcloud_pub_.publish(cloud_msg);
+//             //fill the color info
+//             uint8_t rgb[4];
+//             rgb[0] = cvimg.at<cv::Vec3b>(i,j)[0];
+//             rgb[1] = cvimg.at<cv::Vec3b>(i,j)[1];
+//             rgb[2] = cvimg.at<cv::Vec3b>(i,j)[2];
+//             rgb[3] = 0;
+//             Pointcloud.points[i*Pointcloud.width+j].rgb = *(float *)(rgb);
+//         }
+// #endif
+//     //get the duration....
+//     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+//     std::cout<<"process_time is "<< duration << " second" <<'\n';
+//     //publish pointcloud
+//     pcl::toROSMsg(Pointcloud,cloud_msg);
+//     pointcloud_pub_.publish(cloud_msg);
     //publish param matrix 4*3 = 12
     param_vector.header = img->header;
     param_vector.vector_dim = 12;
